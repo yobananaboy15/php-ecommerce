9226dce2ad4d968f5cca385541cb627b157d703e
Deepthi Ushus <ushusdeepthi@gmail.com>
2021-05-19 11:47:07 +0200

    Add functionality

--- views/include/footer.php
+++ views/include/footer.php
@@ -1,10 +1,13 @@
         <!-- Footer-->
         <footer class="py-5 bg-dark">
-            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2021</p></div>
+            <div class="container">
+                <p class="m-0 text-center text-white">Copyright &copy; Your Website 2021</p>
+            </div>
         </footer>
         <!-- Bootstrap core JS-->
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"></script>
-       
-    </body>
-</html>
+        <script src="https://kit.fontawesome.com/deedf13270.js" crossorigin="anonymous"></script>
+        </body>
+
+        </html>
